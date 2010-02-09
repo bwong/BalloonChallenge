9a474976fedac92cce40cd4e0bd75f94eb5e3b39
Kevin Weiler <Kevin@Kevin-Weilers-MacBook.local>
2010-02-08 23:48:44 -0800

    added a game view with a background picture

---
+++ Classes/GameView.h
@@ -0,0 +1,16 @@
+//
+//  GameView.h
+//  BalloonChallenge
+//
+//  Created by Kevin Weiler on 2/8/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface GameView : UIViewController {
+
+}
+
+@end
