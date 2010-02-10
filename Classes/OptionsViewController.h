f4f25686bdf58e2865df0111297e820646f03db8
Brian Wong <wong.m.brian@gmail.com>
2010-02-09 23:30:55 -0800

    added new view controller classes

---
+++ Classes/OptionsViewController.h
@@ -0,0 +1,16 @@
+//
+//  OptionsViewController.h
+//  BalloonChallenge
+//
+//  Created by Brian Wong on 2/9/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface OptionsViewController : UIViewController {
+
+}
+
+@end
